8be3fdb806d374ad8b47a3b60f30d730aef4bc7a
鈴木孝伸 <>
2021-01-24 14:35:27 +0900

    feature/storybook-test

---
+++ .storybook/main.js
@@ -0,0 +1,40 @@
+const ArcGISPlugin = require('@arcgis/webpack-plugin');
+
+module.exports = {
+  stories: ['../src/components/*.stories.@(tsx|mdx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-essentials',
+    '@storybook/preset-scss',
+  ],
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+    },
+  },
+  webpackFinal(config) {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+          options: {
+            transpileOnly: true,
+          },
+        },
+      ],
+    });
+
+    config.resolve.extensions.push('.ts', '.tsx');
+
+    return config;
+  },
+};
---
+++ src/components/AppTitle.stories.tsx
@@ -0,0 +1,6 @@
+// Button.stories.tsx
+
+import React from 'react';
+import HeaderIcon from './HeaderIcon';
+
+export const showAppTitle = () => <HeaderIcon icon="home" />;
